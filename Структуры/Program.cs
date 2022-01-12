6906163782d83378f12c1f9d7f1b7854005fa0c2
canya <canya@DESKTOP-0RBMO97>
2022-01-12 22:41:46 +0300

    Добавьте файлы проекта.

---
+++ Структуры/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Структуры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Решить линейное уравнение kx+b = 0");
+                Console.WriteLine("Введите коэффициенты уравнения:");
+                Console.Write("Введите 1-й коэффициент: ");
+                int ratio1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите 2-й коэффициент: ");
+                int ratio2 = Convert.ToInt32(Console.ReadLine());
+                Linear_Equation linear = new Linear_Equation(ratio1, ratio2);
+                linear.Root(ratio1, ratio2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Структуры/linear_equation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Структуры
+{
+    //Разработать структуру для решения линейного уравнения 0=kx+b.
+    //Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+    //Для решения уравнения предусмотреть метод Root.Создать экземпляр разработанной структуры.
+    //Осуществить использование экземпляра в программе.
+
+
+     struct Linear_Equation
+    {
+ 
+        public float Ratio1 { get; set; }
+        public float Ratio2 { get; set; }
+        
+        public Linear_Equation(float ratio1, float ratio2)
+        {
+            Ratio1 = ratio1;
+            Ratio2 = ratio2;
+        }
+
+        public void Root(float ratio1, float ratio2) //kx + b = 0: k = ratio1, b = ratio2;
+        {
+            if (ratio1==0)
+            {
+                Console.WriteLine("Коэффициент 1 не может быть равен 0!");
+            }
+            else
+            {
+                float x = (-ratio2 / ratio1);
+                Console.Write("X = {0:F2}", x);
+            }
+                
+        }
+    }
+}
